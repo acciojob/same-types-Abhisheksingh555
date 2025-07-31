0eecf2d23f71252034f57fd0dfed051c0f5b1c96
Abhishek Singh <136239184+Abhisheksingh555@users.noreply.github.com>
2025-07-31 19:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function isSameType(value1, value2) {
-	if(typeof(value1) === typeof(value2)){
-		return true;
-	}else{
-		return  false;
-	}
+  // Special case: check if both are NaN
+  if (Number.isNaN(value1) && Number.isNaN(value2)) {
+    return true;
+  }
 
+  return typeof value1 === typeof value2;
 }
+
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
